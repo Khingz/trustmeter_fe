418fcfaa364d84d479709ffbc5b83189d56f1070
khingz <kingsley995mr@gmail.com>
2025-04-29 07:14:00 +0100

    fix: nav width on mobile screen onscroll

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -21,13 +21,17 @@ const NavBar = () => {
 	return (
 		<nav
 			className={`fixed w-full top-0 left-1/2 transform -translate-x-1/2 z-50 transition-colors duration-300 ${
-				isScrolled ? "bg-[rgba(255,255,255,0.9)] shadow-lg w-[86%] md:w-[80%] top-4 md:top-8 border border-gray-350 rounded-full" : "bg-white"
+				isScrolled
+					? "bg-[rgba(255,255,255,0.9)] shadow-lg w-[85%] md:w-[80%] top-4 md:top-8 border border-gray-350 rounded-full"
+					: "bg-white"
 			}`}
 		>
 			<div className="w-full lg:w-4/5 mx-auto flex justify-between items-center py-[1rem] md:py-[1.5rem] px-[1.5rem] md:gap-1">
 				<div className="">
 					<Link to={"/"}>
-						<h2 className="text-indigo-600 font-bold text-2xl md:text-3xl">TrustMeter</h2>
+						<h2 className="text-indigo-600 font-bold text-2xl md:text-3xl">
+							TrustMeter
+						</h2>
 					</Link>
 				</div>
 				<div className="flex items-center justify-end gap-10">
@@ -44,7 +48,11 @@ const NavBar = () => {
 					<div
 						className={`absolute top-0 w-screen min-h-screen transition-all duration-500 ease-in-out ${
 							isNavOpen ? "left-0" : "-left-[1000px]"
-						} ${isNavOpen && isScrolled ? "left-1/2 transform -translate-x-1/2 top-[-1.05rem]" : ""}`}
+						} ${
+							isNavOpen && isScrolled
+								? "left-1/2 transform -translate-x-1/2 top-[-1.05rem]"
+								: ""
+						}`}
 					>
 						<NavMenuMobile />
 					</div>
